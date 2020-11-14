ad69072c7e20565dff6387fba5ad789c5d3a5df2
Diogo Eichert <diogoeichert@icloud.com>
2020-11-14 11:17:30 +0100

    Update and rename install.sh to update.sh

---
+++ setup-macos/update.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Check for Xcode Command Line Tools
+if ! xcode-select -p > /dev/null ; then
+  xcode-select --install
+
+  while ! xcode-select -p > /dev/null ; do
+    sleep 10
+  done
+fi
+
+# Check for Homebrew
+if ! brew -h > /dev/null ; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+# Install packages
+brew install contacts node
+
+# Setup git
+git config --global user.email "`contacts -Hm -f '%e'`"
+git config --global user.name "`contacts -Hm -f '%n'`"
+
+# Install apps
+brew bundle install --file=~/Documents/Brewfile
+
+# Update bundle
+brew bundle dump --describe --file=~/Documents/Brewfile --force
